be2c6ab317cb1a13447fafac40b2af97bdd441fe
ROTCHILD JOLIBOIS <125093484+Theodor2009@users.noreply.github.com>
2026-07-16 21:39:17 -0300

    Add files via upload

---
+++ rotchild/src/eletroeletronica1.java
@@ -0,0 +1,9 @@
+public class eletroeletronica1 {
+    public static void main() {
+        boolean a = false;
+        boolean b = true;
+        boolean c = a & b;
+
+        System.out.println(c);
+    }
+}
